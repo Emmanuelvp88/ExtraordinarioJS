5f77b09f6ba1c5c7096523bf287ebc8ed42c08b8
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-01 18:52:25 -0600

    Termine la primera prueba

--- contraseña - copia.js
+++ contraseña - copia.js
@@ -1,31 +1,40 @@
 //Pedimos Al usuario ingresar una contraseña
-alert (`Acontinuacion debera ingresar una contraseña que 
-que contenga al menos 8 caracteres, una letra MAYUSCULA,una miniscula y un numero.`);
-let contraseña = prompt("Ingrese su contraseña");
+alert (`Acontinuacion debera ingresar una contraseña que contenga al menos 8 caracteres, una letra MAYUSCULA,una miniscula y un numero.`);
 
 
+//Funcion para validar contraseña
+let validarContraseña =  (contraseña) => {
+    //Validar que contenga minimo 8 carecteres
+    if (contraseña.length <= 8){
+        return false;
+    }
 let letraMayuscula = false;
 let letraMinuscula = false;
 let contieneNumero = false;
-
-//Funcion para validar contraseña
-let validaContraseña (contraseña) => {
-    //Validar que contenga minimo 8 carecteres
-    if (contraseña.length < 8){
-        return false;
-    }
     //Bucle para recorer numero de caracteres
     for (let i = 0; i < contraseña.length; i++){
-        contraseña = contraseña.charAt(i);
+        let caracter = contraseña.charAt(i);
 
         //Validar si contiene mayusculas
-        if(contraseña > "A" && contraseña < "Z"){
+        if(caracter >= "A" && caracter <= "Z"){
             letraMayuscula = true;
-        }else if(contraseña > "a" && contraseña < "z"){
+        }else if(caracter >= "a" && caracter <= "z"){
             letraMinuscula = true;
-        }else if(!isNaN(contraseña)){
+        }else if(!isNaN(caracter)){
             contieneNumero = true;
         }
-
     }
+
+    return letraMayuscula && letraMinuscula && contieneNumero;
 }
+
+let contraseña = prompt("Ingrese su contraseña");
+
+//Mostramos los resultados al usuario
+if (validarContraseña(contraseña)){
+    alert("Lacontraseña cumple contodos los requisitos")
+}else{
+    alert(`La contraseña no cumple con todos los requisitos, favor de revisarla, Contraseña: ${contraseña}`)
+};
+
+
