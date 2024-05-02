601566adcea0689d16529a9be57e9c7b7dc0cc99
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-01 18:06:34 -0600

    Subiendo cambios en la copia

--- contraseña - copia.js
+++ contraseña - copia.js
@@ -1,4 +1,31 @@
 //Pedimos Al usuario ingresar una contraseña
 alert (`Acontinuacion debera ingresar una contraseña que 
-que contenga por lomenos una letra MAYUSCULA, una miniscula y u nnumero.`);
-prompt("Ingrese su contraseña");
+que contenga al menos 8 caracteres, una letra MAYUSCULA,una miniscula y un numero.`);
+let contraseña = prompt("Ingrese su contraseña");
+
+
+let letraMayuscula = false;
+let letraMinuscula = false;
+let contieneNumero = false;
+
+//Funcion para validar contraseña
+let validaContraseña (contraseña) => {
+    //Validar que contenga minimo 8 carecteres
+    if (contraseña.length < 8){
+        return false;
+    }
+    //Bucle para recorer numero de caracteres
+    for (let i = 0; i < contraseña.length; i++){
+        contraseña = contraseña.charAt(i);
+
+        //Validar si contiene mayusculas
+        if(contraseña > "A" && contraseña < "Z"){
+            letraMayuscula = true;
+        }else if(contraseña > "a" && contraseña < "z"){
+            letraMinuscula = true;
+        }else if(!isNaN(contraseña)){
+            contieneNumero = true;
+        }
+
+    }
+}
