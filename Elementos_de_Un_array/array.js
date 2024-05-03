e35318cb16492bc8781364032d632c91e378ac12
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-02 23:49:13 -0600

    subi la nueva carpeta cn los archivos de la leccion 3

---
+++ Elementos_de_Un_array/array.js
@@ -0,0 +1,10 @@
+alert("Este programa te pedira numeros y te regresara la suma de todos ellos.");
+
+//Pedimos la cantidad de numero que desea sumar al usuario
+let cantidadNumeros = prompt("Cuantos numero deseas sumar?");
+  for (i = 0; i < cantidadNumeros; i++){
+    let iterador = prompt(`dime cua es el numero ${i+1}`);
+    let almacenarNumeros =+ [0][iterador];
+  }
+
+alert(almacenarNumeros);
