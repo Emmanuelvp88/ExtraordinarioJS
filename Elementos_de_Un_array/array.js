67ef667e58fad98bf5fc19dd55c2b9d21a38ca64
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-05 21:26:35 -0600

    Subiendo la prueba 3

--- Elementos_de_Un_array/array.js
+++ Elementos_de_Un_array/array.js
@@ -1,10 +1,20 @@
-alert("Este programa te pedira numeros y te regresara la suma de todos ellos.");
+// alert("Este programa te pedira numeros y te regresara la suma de todos ellos.");
+let numeros = [4, 5, 7, 8];
 
-//Pedimos la cantidad de numero que desea sumar al usuario
-let cantidadNumeros = prompt("Cuantos numero deseas sumar?");
-  for (i = 0; i < cantidadNumeros; i++){
-    let iterador = prompt(`dime cua es el numero ${i+1}`);
-    let almacenarNumeros =+ [0][iterador];
+const sumarNumeros = (array) => {
+  for (var iterador of array) {
+    document.write(iterador);
+    // suma += iterador;
   }
+  return suma;
+};
+document.write(sumarNumeros(numeros));
+//Pedimos la cantidad de numero que desea sumar al usuario
+// let cantidadNumeros = prompt("Cuantos numero deseas sumar?");
+//   for (i = 0; i < cantidadNumeros; i++){
+//     let iterador = prompt(`dime cua es el numero ${i+1}`);
+//     let almacenarNumeros =+ [0][iterador];
+//   }
 
-alert(almacenarNumeros);
+
+// alert(almacenarNumeros);
