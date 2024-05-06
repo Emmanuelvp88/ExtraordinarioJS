34137a66a0b7c1870621feb617659cc6751a6b53
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-05 23:41:45 -0600

    tratando de hacer que el programa sea dianamico

--- Elementos_de_Un_array/array.js
+++ Elementos_de_Un_array/array.js
@@ -1,13 +1,20 @@
-// alert("Este programa te pedira numeros y te regresara la suma de todos ellos.");
-let numeros = [4, 5, 7, 8];
+alert("Este programa te pedira numeros y te regresara la suma de todos ellos.");
+let cantidadNumeros = prompt("cuantos numeros deseas capturar?");
+let numeros = [];
+
+for (let i = 0; i < cantidadNumeros.length; i ++){
+    numeros[i] = prompt("ingresa el numero " + (i+1));
+}
 
 const sumarNumeros = (array) => {
+  var suma =  0;
   for (var iterador of array) {
-    document.write(iterador);
-    // suma += iterador;
+    // document.write(iterador);
+    suma += iterador;
   }
   return suma;
 };
+
 document.write(sumarNumeros(numeros));
 //Pedimos la cantidad de numero que desea sumar al usuario
 // let cantidadNumeros = prompt("Cuantos numero deseas sumar?");
