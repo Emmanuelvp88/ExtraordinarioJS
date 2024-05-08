76e46bc47bad103bda5c69109441c23396b88025
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-08 00:04:00 -0600

    Subo el cuarto ejercicio

---
+++ Tabla de Multiplicar/main.js
@@ -0,0 +1,25 @@
+const multiplicar = () =>{
+    let numeroIngresado = parseInt(prompt(`Ingresa un numero del 1 al 10 al cual deseas crear la tabla de multiplicar: `));
+    switch (numeroIngresado){
+        case 1:
+            for (var i = 0; i < 10; i++){
+                var multiplicador =+ (numeroIngresado * 1)
+            }
+        case 3:
+        case 2:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            
+            
+            
+            
+            
+            
+            
+    }
+}
