8a0a85392170209bf9a955538d98d51ac56b8ed3
emmanuelvp88 <emamnuel.villalvap@outlook.es>
2024-05-08 15:33:08 -0600

    Puse todos los blicles el los case actividad 4

--- Tabla de Multiplicar/main.js
+++ Tabla de Multiplicar/main.js
@@ -1,29 +1,51 @@
-const multiplicar = () =>{
-    let numeroIngresado = parseInt(prompt(`Ingresa un numero del 1 al 10 al cual deseas crear la tabla de multiplicar: `));
-    switch (numeroIngresado){
-        
+const multiplicar = () => {
+    let numeroIngresado = parseInt(
+        prompt(
+            `Ingresa un numero del 1 al 10 al cual deseas crear la tabla de multiplicar: `
+        )
+    );
+    switch (numeroIngresado) {
         case 1:
-            for (var i = 0; i <= 10; i++){
-                let multiplicador =+ (numeroIngresado * 1)
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador =+ (numeroIngresado * 1);
             }
         case 2:
-            for (var i = 0; i <= 10; i++){
-                multiplicador
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador =+ (numeroIngresado * 2);
             }
         case 3:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador =+ (numeroIngresado * 3);
+            }
         case 4:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 4);
+            }
         case 5:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 5);
+            }
         case 6:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 6);
+            }
         case 7:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 7);
+            }
         case 8:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 8);
+            }
         case 9:
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 9);
+            }
         case 10:
-            
-            
-            
-            
-            
-            
-            
+            for (var i = 0; i <= 10; i++) {
+                let multiplicador = +(numeroIngresado * 10
+                );
+            }
     }
-}
+
+};
