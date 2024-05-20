f0c2301757d96c65996a62ebd9813048ed468131
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2024-05-20 07:08:05 -0600

    Cambios en ahtml en ponieindo los ID,  stilos para el parrafo y las condicionamas en JS

--- Formulario/main.js
+++ Formulario/main.js
@@ -1,1 +1,29 @@
-const name = document.getElementById("name");
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const pass = document.getElementById("password");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e=>{
+    // document.write("asduasndasdasdasdasds");
+    e.preventDefault();
+    let warnings = "";
+    let entrar = false;
+    let RegexEmail = /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,4})$/
+    parrafo.innertHTML
+    if (nombre.value.length < 6) {
+        warnings += `El nombre no es valido <br>`
+        entrar = true;
+    }
+    if (!RegexEmail.test(email.value)) {
+        warnings += `El email no es valido <br>`
+        entrar = true;
+    }
+    if (pass.value.length < 8) {
+        warnings = + `El password no es valido <br>`
+        entrar = true;
+    }
+    if (entrar) {
+        parrafo.innertHTML = warnings;
+    }
+});
